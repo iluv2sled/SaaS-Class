402ebfa628c57ba89e3ef4def07bd5a1fde9f6e0
Todd Crook <todd.crook@gmail.com>
2012-03-05 00:07:56 +0000

    added case sensitivity

--- HW1_part3.rb
+++ HW1_part3.rb
@@ -2,16 +2,19 @@ def combine_anagrams(words)
   h = Hash.new
 
   words.each do |word|
-    sorted = word.scan(/./).sort.join
+    sorted = word.downcase.scan(/./).sort.join
+    puts sorted
     if(h.has_key?(sorted))
+      puts "has key"
       h[sorted] = h[sorted].insert(h[sorted].length, word)
     else
+      puts "no key"
       h[sorted] = Array.new(1,word)
     end
   end
   return h.values
 end
 
-words = ['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream']
+words = ['cArs', 'for', 'hello', 'potatoes', 'HeLLo', 'racs', 'four', 'scar', 'creams', 'scream']
 puts combine_anagrams(words)
 
