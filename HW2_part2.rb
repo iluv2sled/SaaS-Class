1ada5352055ed3b9e7c134b32c6f5ba4c56cf253
Todd Crook <todd.crook@gmail.com>
2012-03-11 19:33:04 +0000

    add CartesianProduct

---
+++ HW2_part2.rb
@@ -0,0 +1,28 @@
+class CartesianProduct
+  include Enumerable
+  
+  def initialize(a, b)
+    @cart = []
+    a.each do |i|
+      b.each do |j|
+        @cart << [i,j]
+     end 
+    end
+  end
+
+  def each
+    yield @cart
+  end
+end
+
+c = CartesianProduct.new([:a,:b, :c], [4,5])
+c.each { |elt| puts elt.inspect }
+#[:a, 4]
+#[:a, 5]
+#[:b, 4]
+#[:b, 5]
+
+c = CartesianProduct.new([:a,:b], [])
+c.each { |elt| puts elt.inspect }
+# (nothing printed since Cartesian product
+# of anything with an empty collection is empty)
